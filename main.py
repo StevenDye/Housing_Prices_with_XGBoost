e51de8fcc7d1e366320f4bc41d7e3ba4415b8299
Steven Dye <37592040+StevenDye@users.noreply.github.com>
2018-10-07 18:41:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,23 +15,20 @@ ames_features = ['LotArea', 'OverallQual', 'OverallCond', 'FullBath', 'HalfBath'
 X = ames_data[ames_features]
 y = ames_data.SalePrice
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=1)
-
 
 # DEFINING MODEL AND FIT
-ames_model = XGBRegressor(random_state=1).fit(X_train,y_train)
+ames_model = XGBRegressor(random_state=1).fit(X,y)
 
 
 # ERROR CALCULATION
-predicted_home_prices = ames_model.predict(X_train)
-MAE = mean_absolute_error(y_train, predicted_home_prices)
+predicted_home_prices = ames_model.predict(X)
+MAE = mean_absolute_error(y, predicted_home_prices)
 scores = cross_val_score(ames_model, X, y, scoring='neg_mean_absolute_error')
 
 
 # PREDICTIONS FROM TEST DATA
 test = pd.read_csv('input/test.csv')
 test_X = test[ames_features]
-#test_X.fillna(0, inplace=True)
 
 predicted_prices = ames_model.predict(test_X)
 
